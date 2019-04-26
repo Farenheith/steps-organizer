72cc3720aaa56583f21b55f42150fa04b022bead
Thiago Oliveira Santos <thiago.santos@totvs.com.br>
2019-04-26 08:51:18 -0300

    Mudança no organizer, para criar cadeias

---
+++ src/interfaces/2 - domain/models/step-interactor.interface.ts
@@ -0,0 +1,5 @@
+import { IStepChain } from "./step-chain.interface";
+
+export interface IStepsInteractor {
+    children: IStepChain[];
+}
--- src/interfaces/2 - domain/organize-steps-service.interface.ts
+++ src/interfaces/2 - domain/organize-steps-service.interface.ts
@@ -1,6 +1,6 @@
 import { IBaseService } from "base-ddd";
 import { IWorkResume } from "./models/work-resume.interface";
-import { IPlan } from "./models/plan.interface";
+import { IStepsInteractor } from "./models/step-interactor.interface";
 
-export interface IOrganizeStepsService extends IBaseService<IWorkResume, IPlan> {
+export interface IOrganizeStepsService extends IBaseService<IWorkResume, IStepsInteractor> {
 }
